63c0144c658468bf44ed8f9c74b66fade7488d96
Radim Podola <rpodola@gmail.com>
2020-04-18 21:08:04 +0200

    Setup package installation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# cert-validataion-stats
+Cevast: Certificate Validation Statistics Tool
+==============================================
+
+Installation
+------------
+
+You can install Cevast as follows:
+
+    git clone https://github.com/crocs-muni/cert-validation-stats
+    cd cert-validation-stats
+
+    python3.6 setup.py install
+or
+    python3.6 setup.py install --user
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import cevast
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='cevast',
+    version=cevast.__version__,
+    py_modules=['cevast'],
+    author='Radim Podola',
+    author_email='rpodola@gmail.com',
+    description=cevast.__doc__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/crocs-muni/cert-validation-stats',
+    packages=find_packages(),
+    classifiers=[
+         'Programming Language :: Python :: 3',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
