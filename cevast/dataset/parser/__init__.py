a2d7f87fc2fe316a6939cf99545ebbd0d836be77
Radim Podola <rpodola@gmail.com>
2020-05-17 17:35:06 +0200

    Rename RapidParser module

--- cevast/dataset/parser/__init__.py
+++ cevast/dataset/parser/__init__.py
@@ -1,22 +1,24 @@
 """This package provides tools for parsing certificate datasets."""
 
-__all__ = ('RapidParser', 'getParser', 'getParserFromConfig')
+__all__ = ('RapidParser', 'get_parser_class', 'init_parser_from_cfg')
 __version__ = '0.1'
 __author__ = 'Radim Podola'
 
 from cevast.dataset import DatasetType
-from .rapid7 import RapidParser
+from .rapid import RapidParser
 
 
-def getParserClass(dataset_type):
-    if(dataset_type == DatasetType.RAPID):
+def get_parser_class(dataset_type: DatasetType):
+    """Provide parser class object based on the required dataset type"""
+    if dataset_type == DatasetType.RAPID:
         return RapidParser
-    else:
-        return None
+
+    return None
 
 
-def getParserFromConfig(config):
-    if(config["type"] == DatasetType.RAPID):
-        return RapidParser.fromConfig(config)
-    else:
-        return None
+def init_parser_from_cfg(config):
+    """Instantiate a parser class from the configuration file"""
+    if config["type"] == DatasetType.RAPID:
+        return RapidParser.from_config(config)
+
+    return None
