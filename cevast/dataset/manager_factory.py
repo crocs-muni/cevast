8d1bc56baf08f2386e94e48c598ded596121c48a
Radim Podola <rpodola@gmail.com>
2020-06-26 18:00:55 +0200

    Try IMPORTANT reST directive

--- cevast/dataset/manager_factory.py
+++ cevast/dataset/manager_factory.py
@@ -6,7 +6,13 @@ from . import managers
 
 
 class DatasetManagerFactory:
-    """Class implementing factory design patter for creation of DatasetManager objects."""
+    """
+    Factory class providing the specific DatasetManager class based on DatasetType.
+
+    .. IMPORTANT:: DatasetManager classes are registered automatically. To add a new
+    specialized DatasetManager class, it must inherit DatasetManager and be placed
+    in "cevast.dataset.managers" package.
+    """
 
     __classes = {}
 
@@ -15,9 +21,6 @@ class DatasetManagerFactory:
         """
         Automatically initialize lookup dictionary with subclasses of DatasetManager class
         that are visible to the Python interpreter (obtained from `type.__subclasses__()`).
-
-        .. DANGER:: To be automatically identified, the specialized DatasetManager class
-        must inherit DatasetManager and be placed in "cevast.dataset.managers" package.
         """
         for manager_class in managers.DatasetManager.__subclasses__():
             if hasattr(manager_class, 'dataset_type'):
@@ -25,7 +28,7 @@ class DatasetManagerFactory:
 
     @classmethod
     def get_manager(cls, dataset_type: Union[DatasetType, str]) -> Type[managers.DatasetManager]:
-        """Instantiate a corresponding DatasetManager class based on `dataset_type`."""
+        """Return a corresponding DatasetManager class based on `dataset_type`."""
         if not cls.__classes:
             cls.__load_classes()
 
